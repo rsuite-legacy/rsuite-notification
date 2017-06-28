652e64f15c44653fed2764d4bed217ada0262a6d
Superman <supermanchc@gmail.com>
2017-06-28 17:28:32 +0800

    Add Notice

---
+++ src/Notice.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const propTypes = {
+  duration: PropTypes.number,
+  content: PropTypes.any,
+  onClose: PropTypes.func
+};
+
+const defaultProps = {};
+
+class Notice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    const { duration } = this.props;
+    if (duration) {
+      this.closeTimer = setTimeout(() => {
+        this.close();
+      }, duration * 1000);
+    }
+  }
+
+  clearCloseTimer = () => {
+    if (this.closeTimer) {
+      clearTimeout(this.clearCloseTimer);
+      this.clearCloseTimer = null;
+    }
+  }
+
+  close = () => {
+    this.clearCloseTimer();
+    this.props.onClose();
+  }
+
+  render() {
+    const { prefixCls, closable, className, content, style } = this.props;
+    const componentClass = `${prefixCls}-notice`;
+    const className = {
+      [`${componentClass}`]: true,
+      [`${componentClass}-closable`]: closable,
+      [className]: !!props.className
+    };
+
+    return (
+      <div className={className} style={style}>
+        <div className={`${componentClass}-content`}>{content}</div>
+        {closable && <a onClick={this.close} className={`${componentClass}-close`}>
+          <span className={`${componentClass}-close-x`}></span>
+        </a>}
+      </div>
+    );
+  }
+}
+
+Notice.propTypes = propTypes;
+
+Notice.defaultProps = defaultProps;
+
+export default Notice;
