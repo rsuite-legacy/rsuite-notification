df2722a9ef5d8ce08b801dbf8b81aa63419252df
Superman <supermanchc@gmail.com>
2017-11-16 15:26:14 +0800

    Update test case

--- test/Notify.spec.js
+++ test/Notify.spec.js
@@ -1,5 +1,6 @@
 import { Notify } from '../src';
 
+
 const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
 
 describe(' Notify Component', () => {
@@ -41,5 +42,62 @@ describe(' Notify Component', () => {
     await delay(1000);
     expect(document.querySelectorAll('.rsuite-notification-success').length).toBe(0);
   });
+
+  it('success notify should be render, after 2s, it is gone', async () => {
+    Notify.success({
+      title: 'Notify',
+      description: 'desc',
+      duration: 2,
+      key: 0
+    });
+    expect(document.querySelectorAll('.rsuite-notification-success').length).toBe(1);
+
+    await delay(3000);
+    expect(document.querySelectorAll('.rsuite-notification-success').length).toBe(0);
+  });
+
+  it('error notify should be render', async () => {
+    Notify.error({
+      title: 'Notify',
+      description: 'desc',
+      duration: 0,
+      key: 0
+    });
+    expect(document.querySelectorAll('.rsuite-notification-error').length).toBe(1);
+
+    Notify.remove(0);
+    await delay(1000);
+    expect(document.querySelectorAll('.rsuite-notification-error').length).toBe(0);
+  });
+
+  it('info notify should be render', async () => {
+    Notify.info({
+      title: 'Notify',
+      description: 'desc',
+      duration: 0,
+      key: 0
+    });
+    expect(document.querySelectorAll('.rsuite-notification-info').length).toBe(1);
+
+    Notify.remove(0);
+    await delay(1000);
+    expect(document.querySelectorAll('.rsuite-notification-info').length).toBe(0);
+  });
+
+  it('warning notify should be render', async () => {
+    Notify.warning({
+      title: 'Notify',
+      description: 'desc',
+      duration: 0,
+      key: 0
+    });
+    expect(document.querySelectorAll('.rsuite-notification-warning').length).toBe(1);
+
+    Notify.remove(0);
+    await delay(1000);
+    expect(document.querySelectorAll('.rsuite-notification-warning').length).toBe(0);
+  });
+
+
 });
 
