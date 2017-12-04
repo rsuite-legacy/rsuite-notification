f6163be29b20e533e8621ecc7b206b1ad74d5026
Superman <supermanchc@gmail.com>
2017-12-04 16:26:20 +0800

    Update docs

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 [![Travis](https://img.shields.io/travis/rsuite/rsuite-notification.svg)](https://travis-ci.org/rsuite/rsuite-notification) [![npm](https://img.shields.io/npm/v/rsuite-notification.svg)](https://www.npmjs.com/package/rsuite-notification) [![Coverage Status](https://coveralls.io/repos/github/rsuite/rsuite-notification/badge.svg?branch=master)](https://coveralls.io/github/rsuite/rsuite-notification?branch=master)
 # rsuite-notification
 `rsuite-notification` 提供了 `Alert` 和 `Nofity` 两个组件。
-安装：
+## 快速开始
+### 安装
 
 ```javascript
 npm install rsuite-notification
 ```
 
-引入less文件：
+### 引入less文件：
 
 ```
 @import "~rsuite-notification/lib/less/index";
--- docs/app.js
+++ docs/app.js
@@ -3,11 +3,10 @@ import { Header, Navbar, Nav, Row, Col } from 'rsuite';
 import Affix from 'rsuite-affix';
 import { Markdown } from 'markdownloader';
 import CodeView from 'react-code-view';
+import { PageContainer } from 'rsuite-docs';
 import 'react-code-view/lib/less/index.less';
 import { PLACEMENT_TYPES } from '../src/constants/index';
 import { Alert, Notify } from '../src';
-import AlertDemo from './component/Alert';
-import NotifyDemo from './component/Notify';
 import '../src/less/index.less';
 import './less/index.less';
 
@@ -21,74 +20,42 @@ const babelOptions = {
 class App extends Component {
   render() {
     return (
-      <div className="doc-page">
-        <Header inverse>
-          <div className="container">
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="#"><span className="prefix">R</span>Suite Notification</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-              <Nav>
-                <Nav.Item href="#API">API</Nav.Item>
-              </Nav>
-              <Nav pullRight>
-                <Nav.Item href="https://rsuite.github.io">RSuite</Nav.Item>
-                <Nav.Item href="https://github.com/rsuite/rsuite-notification">GitHub</Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-        </Header>
-        <div className="container">
-          <Row>
-            <Col md={2} xsHidden smHidden>
-              <Affix offsetTop={70}>
-                <Nav pills stacked className="sidebar">
-                  <Nav.Item href="#README"># 概述</Nav.Item>
-                  <Nav.Item href="#examples"># 示例</Nav.Item>
-                  <Nav.Item href="#alert"># Alert</Nav.Item>
-                  <Nav.Item href="#notiy"># Notification</Nav.Item>
-                  <Nav.Item href="#API"># API</Nav.Item>
-                </Nav>
-              </Affix>
-            </Col>
-            <Col md={10}>
-              <a id="README" className="target-fix" />
-              <Markdown>{require('../README.md')}</Markdown>
-              <hr id="alert" className="target-fix" />
-              <Row>
-                <Col md={12}>
-                  <CodeView
-                    source={require('./md/Alert.md')}
-                    dependencies={{
-                      Alert
-                    }}
-                    babelTransformOptions={babelOptions}
-                  />
-                </Col>
-              </Row>
-              <hr id="notify" className="target-fix" />
-              <Row>
-                <Col md={12}>
-                  <CodeView
-                    source={require('./md/Notify.md')}
-                    dependencies={{
-                      Notify
-                    }}
-                    babelTransformOptions={babelOptions}
-                  />
-                </Col>
-              </Row>
-              <h2 id="API"><code>{'API'}</code></h2>
-              <Markdown>
-                {require('./md/props.md')}
-              </Markdown>
-            </Col>
-          </Row>
-        </div>
-      </div>
+      <PageContainer
+        activeKey="Notification"
+        githubURL="https://github.com/rsuite/rsuite-notification"
+      >
+        <a id="README" className="target-fix" />
+        <Markdown>{require('../README.md')}</Markdown>
+
+        <h2>示例</h2>
+        <Row>
+          <Col md={12}>
+            <CodeView
+              source={require('./md/Alert.md')}
+              dependencies={{
+                Alert
+              }}
+              babelTransformOptions={babelOptions}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={12}>
+            <CodeView
+              source={require('./md/Notify.md')}
+              dependencies={{
+                Notify
+              }}
+              babelTransformOptions={babelOptions}
+            />
+          </Col>
+        </Row>
+        <h2 id="API">API</h2>
+        <Markdown>
+          {require('./md/props.md')}
+        </Markdown>
+      </PageContainer>
     );
   }
 }
